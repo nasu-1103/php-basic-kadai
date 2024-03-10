a78f62ceb561d5fb8aa16351531412272219a921
nasu-1103 <156154688+nasu-1103@users.noreply.github.com>
2024-03-10 14:44:16 +0900

    kadai_005をアップロード

---
+++ kadai_005/score.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>kadai_005</title>
+</head>
+
+<body>
+    <p>
+       <?php
+       $score_total = 80 + 60 + 55 + 40 + 100 + 25 + 80 + 95 + 30 + 60;
+       echo $score_total / 10
+       ?>
+    </p>
+</body>
+</html>
+
+
+
