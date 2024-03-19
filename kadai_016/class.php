ab427d4025f21213a73e389101eb6ab35e757f14
nasu-1103 <156154688+nasu-1103@users.noreply.github.com>
2024-03-19 10:55:51 +0900

    kadai_016をアップロード

---
+++ kadai_016/class.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>kadai_016</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    // Foodクラスを定義する
+    class Food { 
+      // プロパティを定義する
+      private $name;
+      private $price;
+
+      // メソッドを定義する
+      public function show_price() {
+        echo $this->price . '<br>';
+      }
+
+      // コンストラクタを定義する
+      public function __construct(string $name, int $price) {
+        $this->name = $name;
+        $this->price = $price;
+      }
+    }
+
+    // インスタンス化する
+    $food = new Food('potato', 250);
+
+    // インスタンス$foodの各プロパティを出力する
+    print_r($food);
+    echo '<br>';
+
+    // Animalクラスを定義する
+    class Animal {
+      // プロパティを定義する
+      private $name;
+      private $height;
+      private $weight;
+
+      // メソッドを定義する
+      public function show_height() {
+        echo $this->height . '<br>';
+      }
+
+      // コンストラクタを定義する
+      public function __construct(string $name, int $height, int $weight) {
+        $this->name = $name;
+        $this->height = $height;
+        $this->weight = $weight;
+      }
+    }
+      // インスタンス化する
+      $animal = new Animal('dog', 60, 5000);
+
+      // インスタンス$animalの各プロパティを出力する
+      print_r($animal);
+      echo '<br>';
+
+      // メソッドにアクセスして実行する
+      $food->show_price();
+      $animal->show_height();
+    ?>
+  </p>
+</body>
+</html>
